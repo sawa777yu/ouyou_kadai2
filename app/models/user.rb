01dc3d98fff8be0a1ac1057b5064ee5b1575279e
sawa777yu <sawa.oranger7@gmail.com>
2021-09-02 09:25:29 +0000

    [Finish]ouyou_kadai4

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,34 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :follower_of_relationships, class_name: "Relationship", foreign_key:"follower_id", dependent: :destroy
+  # :follower_of_relationships =  多分この文字はわかりやすければ何でも良い。これを使って後述の値を呼び出せる？
+  # class_name: "Relationships" = has_manyとかの後ろには本来はテーブル名とかが入ってくるのだと思うが中間リレーションを使った時は別の名前を当てる（？）ので
+  # ここでRelationshipのもテーブルを見てくださいねと定義する
+  # foreign_key:"follower_id" = class_nameで指定したテーブルのfollower_idというカラムを参照してくださいねという意味
+  has_many :followings, through: :follower_of_relationships, source: :followed
+  has_many :followed_of_relationships, class_name: "Relationship", foreign_key:"followed_id", dependent: :destroy
+  has_many :followers, through: :followed_of_relationships, source: :follower
+
+# 下記はインスタンスメソッドという
+# コントローラで呼び出すことのできる、モデルに書くメソッドのこと
+  def follow(user_id)
+    follower_of_relationships.create(followed_id: user_id)
+    # 上記のhas_many :follower_of_relationshipsを参照している
+  end
+
+  def unfollow(user_id)
+    follower_of_relationships.find_by(followed_id: user_id).destroy
+  end
+
+  def following?(user)
+    followings.include?(user)
+  end
+
   attachment :profile_image, destroy: false
 
   validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
   validates :introduction, length: {maximum: 50}
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   root 'homes#top'
   get 'home/about' => 'homes#about'
   devise_for :users
-  resources :users,only: [:show,:index,:edit,:update,:create]
+  resources :users,only: [:show,:index,:edit,:update,:create] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
   resources :books do
     resource :favorites,only: [:create, :destroy]
     resources :book_comments,only: [:create, :destroy]
